c4b9a7a7fc2ce19c115624a72978241df11bd6d0
noahpratt <noahlkpratt@gmail.com>
2020-04-14 22:30:06 -0400

    April 14, 2020

--- nav.js
+++ nav.js
@@ -11,6 +11,15 @@ window.onload = function() {checkScroll()};
 window.onscroll = function() {checkScroll()};
 let yPosition = 0;
 let secondNav = document.getElementById("secondary-nav");
+let docHeight = document.body.scrollHeight;
+let winHeight = window.innerHeight;
+
+let sideNav = document.getElementById("floating-nav");
+
+var loadTimer = setTimeout(function(){
+  docHeight = document.body.scrollHeight;
+  console.log("Hello!");
+}, 2000);
 
 function checkScroll() {
   yPosition = document.documentElement.scrollTop;
@@ -18,5 +27,12 @@ function checkScroll() {
     secondNav.style.opacity = "1";
   } else if (yPosition < 80) {
     secondNav.style.opacity = "0";
+  }
+  if (yPosition + winHeight + 140 < docHeight) {
+    sideNav.style.top = yPosition + (window.innerHeight/4) + "px";
+    sideNav.style.height = sideNav.style.height - 300;
+  } else {
+    console.log("Doc Height", docHeight);
+    console.log("Combined: ", yPosition + winHeight);
   }
 }
