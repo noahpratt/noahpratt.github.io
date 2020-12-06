26b0a41ad14fd3e376d6305dd5bfbd49e03253c0
noahpratt <56092410+noahpratt@users.noreply.github.com>
2020-12-06 03:05:39 -0500

    major update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function myFunction() {
+  console.log("..");
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
@@ -6,6 +7,14 @@ function myFunction() {
     x.style.display = "block"
   }
 }
+
+var links = document.getElementsByClassName("project-grid-item");
+for(let i = 0; i < links.length; i++) {
+ links[i].addEventListener("click", function(){
+   window.location = this.dataset.link;
+ });
+};
+
 
 window.onload = function() {checkScroll()};
 window.onscroll = function() {checkScroll()};
@@ -20,3 +29,13 @@ function checkScroll() {
     secondNav.style.opacity = "0";
   }
 }
+
+window.onresize = function() {
+  var w = window.outerWidth;
+  var menu = document.getElementById("myLinks");
+  if (w > 726) {
+    if (menu.style.display === "none") {
+      menu.style.display = "block";
+    }
+  }
+};
